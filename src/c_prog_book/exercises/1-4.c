8cae72efce4991e352f3c0998d1b6ea6752e2cb7
mbrignall <martinaloysiusbrignall@gmail.com>
2025-12-22 22:10:39 +0000

    Refactored old and finished Temp section

--- src/c_prog_book/exercises/1-4.c
+++ src/c_prog_book/exercises/1-4.c
@@ -3,20 +3,15 @@
 
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
-  float fahr, celsius;
-  float lower, upper, step;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahr = lower;
-  printf("Celsius\tFahr\n");
-  while (fahr <= upper) {
-    celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%6.1f\t%3.0f\n", celsius, fahr);
-    fahr = fahr + step;
-  }
+  int fahr;
+  printf("Fahr\tCelsius\n");
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+    printf("%6.1f\t%3d\n", (5.0/9.0)*(fahr-32), fahr);
 }
 
--- src/c_prog_book/exercises/1-5.c
+++ src/c_prog_book/exercises/1-5.c
@@ -1,5 +1,6 @@
-/* Exercise 1-3 */
+/* Exercise 1-5 */
 /* Temp Conversion calc farenheit to celsius */
+/* REVERSED */
 
 #include <stdio.h>
 
@@ -12,6 +13,7 @@ int main() {
   int fahr;
   
   printf("Fahr\tCelsius\n");
-  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
     printf("%3d\t%6.1f\n", fahr, (5.0/9.0) * (fahr-32));
 }
+
