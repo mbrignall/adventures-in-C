be677f2c41737266a76291f4f2179ef5a2bc2012
mbrignall <martinaloysiusbrignall@gmail.com>
2025-12-22 23:19:27 +0000

    New exercises, updated README

---
+++ src/c_prog_book/exercises/1-6.c
@@ -0,0 +1,11 @@
+/* Exercise 1-6 */
+/* Verify the expression */
+
+#include <stdio.h>
+
+int main() {
+  int c;
+
+  c = getchar() != EOF;
+  printf("%d\n", c);
+}
---
+++ src/c_prog_book/exercises/1-7.c
@@ -0,0 +1,11 @@
+/* Exercise 1-7 */
+/* Print value of EOF */
+
+#include <stdio.h>
+
+int main() {
+  int c;
+
+  c = EOF;
+  printf("%d\n", c);
+}
